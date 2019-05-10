969625dfd01e284b78598f94336f1b6cb9727706
Ye Lyu <y.lyu@utwente.nl>
2019-05-10 17:28:09 +0800

    Finish lighthead rfcn model

---
+++ lib/model/faster_rcnn/rfcn.py
@@ -0,0 +1,207 @@
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.models as models
+from torch.autograd import Variable
+import numpy as np
+from model.utils.config import cfg
+from model.rpn.rpn import _RPN
+from model.roi_pooling.modules.roi_pool import _RoIPooling
+from model.psroi_pooling.modules.psroi_pool import PSRoIPool
+from model.roi_crop.modules.roi_crop import _RoICrop
+from model.roi_align.modules.roi_align import RoIAlignAvg
+from model.rpn.proposal_target_layer_cascade import _ProposalTargetLayer
+import time
+import pdb
+from model.utils.net_utils import _smooth_l1_loss, _crop_pool_layer, _affine_grid_gen, _affine_theta
+import math
+
+class _global_context_layer(nn.Module):
+    def __init__(self, c_in, c_out, c_mid=256, ks=15):
+        # the c_out should be set as multiple of pooled_size*pooled_size .
+        super(_global_context_layer, self).__init__()
+
+        self.c_out = c_out
+        self.c_mid = c_mid
+        self.ks = ks
+        # define convolution ops.
+        self.row_prev = nn.Conv2d(c_in, c_mid , kernel_size=(ks,1))
+        self.row_post = nn.Conv2d(c_mid,c_out , kernel_size=(1,ks))
+        self.col_prev = nn.Conv2d(c_in, c_mid , kernel_size=(1,ks))
+        self.col_post = nn.Conv2d(c_mid,c_out , kernel_size=(ks,1))
+        self._init_weights()
+
+    def forward(self, feature):
+        f_row = self.row_prev(feature)
+        f_col = self.col_prev(feature)
+        f_row = self.row_post(f_row)
+        f_col = self.col_post(f_col)
+        return f_row+f_col
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+
+# rfcn of light head version.
+class _rfcn_lighthead(nn.Module):
+    """ faster RCNN """
+    def __init__(self, classes, class_agnostic, mid_c = 10):
+        super(_rfcn_lighthead, self).__init__()
+        self.classes = classes
+        self.mid_c = mid_c # mid_c is the output dim of psroi pooling.
+        self.n_classes = len(classes)
+        self.class_agnostic = class_agnostic
+        # loss
+        self.RCNN_loss_cls = 0
+        self.RCNN_loss_bbox = 0
+
+        # define rpn
+        self.RCNN_rpn = _RPN(self.dout_base_model)
+        self.RCNN_proposal_target = _ProposalTargetLayer(self.n_classes)
+        #self.RCNN_roi_pool = _RoIPooling(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0)
+        #self.RCNN_roi_align = RoIAlignAvg(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0)
+        self.RCNN_psroi_pool = PSRoIPool(cfg.POOLING_SIZE, cfg.POOLING_SIZE, 1.0/16.0, cfg.POOLING_SIZE, mid_c)
+
+        self.grid_size = cfg.POOLING_SIZE * 2 if cfg.CROP_RESIZE_WITH_MAX_POOL else cfg.POOLING_SIZE
+        self.RCNN_roi_crop = _RoICrop()
+
+        # The input channel is set mannually since we use resnet101 only.
+        # c_out is set to 10*ps*ps. c_mid is set to 256.
+        core_depth = 10
+        self.g_ctx = _global_context_layer(2048, core_depth*cfg.POOLING_SIZE*cfg.POOLING_SIZE, 256, 15)
+
+        # fc layer for roi-wise prediction.
+        # roi_mid_c in the original paper is 2048. Since we have fewer classes, it is reduced to 1024.
+        roi_mid_c = 1024
+        self.fc_roi = nn.Linear(core_depth, roi_mid_c)
+
+
+    def forward(self, im_data, im_info, gt_boxes, num_boxes):
+        batch_size = im_data.size(0)
+
+        im_info = im_info.data
+        gt_boxes = gt_boxes.data
+        num_boxes = num_boxes.data
+
+        # feed image data to base model to obtain base feature map
+        base_feat = self.RCNN_base(im_data)
+
+        # feed base feature map tp RPN to obtain rois
+        rois, rpn_loss_cls, rpn_loss_bbox = self.RCNN_rpn(base_feat, im_info, gt_boxes, num_boxes)
+
+        # if it is training phrase, then use ground trubut bboxes for refining
+        if self.training:
+            roi_data = self.RCNN_proposal_target(rois, gt_boxes, num_boxes)
+            rois, rois_label, rois_target, rois_inside_ws, rois_outside_ws = roi_data
+
+            rois_label = Variable(rois_label.view(-1).long())
+            rois_target = Variable(rois_target.view(-1, rois_target.size(2)))
+            rois_inside_ws = Variable(rois_inside_ws.view(-1, rois_inside_ws.size(2)))
+            rois_outside_ws = Variable(rois_outside_ws.view(-1, rois_outside_ws.size(2)))
+        else:
+            rois_label = None
+            rois_target = None
+            rois_inside_ws = None
+            rois_outside_ws = None
+            rpn_loss_cls = 0
+            rpn_loss_bbox = 0
+
+        rois = Variable(rois)
+        # do roi pooling based on predicted rois
+
+        '''
+        if cfg.POOLING_MODE == 'crop':
+            # pdb.set_trace()
+            # pooled_feat_anchor = _crop_pool_layer(base_feat, rois.view(-1, 5))
+            grid_xy = _affine_grid_gen(rois.view(-1, 5), base_feat.size()[2:], self.grid_size)
+            grid_yx = torch.stack([grid_xy.data[:,:,:,1], grid_xy.data[:,:,:,0]], 3).contiguous()
+            pooled_feat = self.RCNN_roi_crop(base_feat, Variable(grid_yx).detach())
+            if cfg.CROP_RESIZE_WITH_MAX_POOL:
+                pooled_feat = F.max_pool2d(pooled_feat, 2, 2)
+        elif cfg.POOLING_MODE == 'align':
+            pooled_feat = self.RCNN_roi_align(base_feat, rois.view(-1, 5))
+        elif cfg.POOLING_MODE == 'pool':
+            pooled_feat = self.RCNN_roi_pool(base_feat, rois.view(-1,5))
+        '''
+        # we need layer 4 before pooling op.
+        base_feat = self.RCNN_top(base_feat)
+
+        # ctx op layer here.
+        base_feat = self.g_ctx(base_feat)
+
+        if cfg.POOLING_MODE == 'pspool':
+            pooled_feat = self.RCNN_psroi_pool(base_feat, rois.view(-1, 5))
+        else:
+            raise 'Other pooling methods are not supported.'
+
+        # feed pooled features to top model
+        # pooled_feat = self._head_to_tail(pooled_feat)
+
+        # before using the pooled feat, we apply a linear layer.
+        pooled_feat = pooled_feat.view(pooled_feat.size(0), -1)
+        pooled_feat = self.fc_roi(pooled_feat)
+        # compute bbox offset
+        bbox_pred = self.RCNN_bbox_pred(pooled_feat)
+        if self.training and not self.class_agnostic:
+            # select the corresponding columns according to roi labels
+            bbox_pred_view = bbox_pred.view(bbox_pred.size(0), int(bbox_pred.size(1) / 4), 4)
+            bbox_pred_select = torch.gather(bbox_pred_view, 1, rois_label.view(rois_label.size(0), 1, 1).expand(rois_label.size(0), 1, 4))
+            bbox_pred = bbox_pred_select.squeeze(1)
+
+        # compute object classification probability
+        cls_score = self.RCNN_cls_score(pooled_feat)
+        cls_prob = F.softmax(cls_score, 1)
+
+        RCNN_loss_cls = 0
+        RCNN_loss_bbox = 0
+
+        if self.training:
+            # classification loss
+            RCNN_loss_cls = F.cross_entropy(cls_score, rois_label)
+
+            # bounding box regression L1 loss
+            RCNN_loss_bbox = _smooth_l1_loss(bbox_pred, rois_target, rois_inside_ws, rois_outside_ws)
+
+            rpn_loss_cls = torch.unsqueeze(rpn_loss_cls, 0)
+            rpn_loss_bbox = torch.unsqueeze(rpn_loss_bbox, 0)
+            RCNN_loss_cls = torch.unsqueeze(RCNN_loss_cls, 0)
+            RCNN_loss_bbox = torch.unsqueeze(RCNN_loss_bbox, 0)
+
+
+        cls_prob = cls_prob.view(batch_size, rois.size(1), -1)
+        bbox_pred = bbox_pred.view(batch_size, rois.size(1), -1)
+
+        return rois, cls_prob, bbox_pred, rpn_loss_cls, rpn_loss_bbox, RCNN_loss_cls, RCNN_loss_bbox, rois_label
+
+    def _init_weights(self):
+        def normal_init(m, mean, stddev, truncated=False):
+            """
+            weight initalizer: truncated normal and random normal.
+            """
+            # x is a parameter
+            if truncated:
+                m.weight.data.normal_().fmod_(2).mul_(stddev).add_(mean) # not a perfect approximation
+            else:
+                m.weight.data.normal_(mean, stddev)
+                m.bias.data.zero_()
+
+        normal_init(self.RCNN_rpn.RPN_Conv, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RCNN_rpn.RPN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RCNN_rpn.RPN_bbox_pred, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RCNN_cls_score, 0, 0.01, cfg.TRAIN.TRUNCATED)
+        normal_init(self.fc_roi, 0, 0.005, cfg.TRAIN.TRUNCATED)
+        normal_init(self.RCNN_bbox_pred, 0, 0.001, cfg.TRAIN.TRUNCATED)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+
+    def create_architecture(self):
+        self._init_modules()
+        self._init_weights()
+
+
