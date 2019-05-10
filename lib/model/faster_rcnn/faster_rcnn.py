acdefbb8c30a43214ed5cde98ad6a3c1120810ab
Ye Lyu <y.lyu@utwente.nl>
2019-05-10 17:29:02 +0800

    reformat

--- lib/model/faster_rcnn/faster_rcnn.py
+++ lib/model/faster_rcnn/faster_rcnn.py
@@ -80,7 +80,7 @@ class _fasterRCNN(nn.Module):
         elif cfg.POOLING_MODE == 'align':
             pooled_feat = self.RCNN_roi_align(base_feat, rois.view(-1, 5))
         elif cfg.POOLING_MODE == 'pool':
-            pooled_feat = self.RCNN_roi_pool(base_feat, rois.view(-1,5))
+            pooled_feat = self.RCNN_roi_pool(base_feat, rois.view(-1, 5))
 
         # feed pooled features to top model
         pooled_feat = self._head_to_tail(pooled_feat)
